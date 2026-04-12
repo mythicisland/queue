b15c1238886da5477a105b85136ee27a972918d8
xXJanisXx <Janis.Minecrafto@gmx.de>
2026-04-12 13:32:07 +0200

    feat: better messages

--- queue-shared/src/main/kotlin/net/mythicisland/queue/shared/message/Messages.kt
+++ queue-shared/src/main/kotlin/net/mythicisland/queue/shared/message/Messages.kt
@@ -7,7 +7,7 @@ typealias Messages = Map<QueueStatus, String>
 
 val defaultMessages: Messages = mapOf(
     QueueStatus.NOT_ENOUGH_PLAYERS to "<color:#cbd5e1>Waiting for players (<queue_players>/<queue_max_capacity>)".toMiniFont(),
-    QueueStatus.WAITING_COUNTDOWN to ("<color:#22c55e>Found Match!</color> <color:#cbd5e1>Starting in <queue_waiting_countdown_seconds>s (<queue_players>/<queue_max_capacity>)").toMiniFont(),
+    QueueStatus.WAITING_COUNTDOWN to ("<color:#22c55e>Found Match!</color> <color:#cbd5e1>Still waiting for players <queue_waiting_countdown_seconds>s (<queue_players>/<queue_max_capacity>)").toMiniFont(),
     QueueStatus.SEARCHING_SERVER to "<color:#cbd5e1>Searching for a <queue_type> server (<queue_players>/<queue_max_capacity>)".toMiniFont(),
     QueueStatus.WAITING_FOR_SERVER to "<color:#cbd5e1>Waiting for a <queue_type> server (<queue_players>/<queue_max_capacity>)".toMiniFont(),
     QueueStatus.SERVER_READY to ("<color:#22c55e>Server found!</color> <color:#cbd5e1>(<queue_players>/<queue_max_capacity>)").toMiniFont(),
